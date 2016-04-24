6bb90375f4efb2f390a61ede0df70eea1ef1f50f
Grant Ammons <gammons@gmail.com>
2016-04-24 10:04:53 -0400

    Read from user's home directory

--- file_store.go
+++ file_store.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"os/user"
 )
 
 type FileStore struct {
@@ -12,18 +14,21 @@ type FileStore struct {
 }
 
 func NewFileStore() *FileStore {
-	return &FileStore{FileLocation: "todos.json"}
+	usr, _ := user.Current()
+	return &FileStore{FileLocation: usr.HomeDir + "/.todos.json"}
 }
 
 func (f *FileStore) Load() {
 	data, err := ioutil.ReadFile(f.FileLocation)
 	if err != nil {
 		fmt.Println("Error reading file", err)
+		os.Exit(1)
 	}
 
 	jerr := json.Unmarshal(data, &f.Data)
 	if jerr != nil {
 		fmt.Println("Error reading json data", jerr)
+		os.Exit(1)
 	}
 
 }
--- file_store_test.go
+++ file_store_test.go
@@ -1,9 +1,14 @@
 package todolist
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFileStore(t *testing.T) {
+	assert := assert.New(t)
 	store := &FileStore{FileLocation: "todos.json"}
 	store.Load()
-	store.Data[0]
+	assert.Equal(store.Data[0].Subject, "this is the first subject", "")
 }
