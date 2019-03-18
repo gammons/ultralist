dd6ed7e658e0dfef6d6847fe8c3c0960e2df01e6
Grant Ammons <gammons@gmail.com>
2019-03-18 07:06:41 -0400

    use colors idempotently
    this change does not manipulate any color variables, so we can guarantee printing will work consistently.
    
    The downside is slight increased duplication in `screen_printer.go`.
    
    This fixes #166 and also removes use of italics (since they have a problem working in tmux)

--- ultralist/screen_printer.go
+++ ultralist/screen_printer.go
@@ -14,13 +14,20 @@ import (
 )
 
 var (
-	blue    = color.New(color.FgBlue)
-	cyan    = color.New(color.FgCyan)
-	magenta = color.New(color.FgMagenta)
-	nocolor = color.New()
-	red     = color.New(color.FgRed)
-	white   = color.New(color.FgWhite)
-	yellow  = color.New(color.FgYellow)
+	blue            = color.New(color.FgBlue)
+	blueBold        = color.New(color.FgBlue, color.Bold)
+	cyan            = color.New(color.FgCyan)
+	magenta         = color.New(color.FgMagenta)
+	magentaBold     = color.New(color.FgMagenta, color.Bold)
+	nocolor         = color.New()
+	red             = color.New(color.FgRed)
+	redBold         = color.New(color.FgRed, color.Bold)
+	white           = color.New(color.FgWhite)
+	whiteBold       = color.New(color.FgWhite, color.Bold)
+	yellow          = color.New(color.FgYellow)
+	yellowBold      = color.New(color.FgYellow, color.Bold)
+	projectRegex, _ = regexp.Compile(`\+[\p{L}\d_]+`)
+	contextRegex, _ = regexp.Compile(`\@[\p{L}\d_]+`)
 )
 
 // ScreenPrinter is the default struct of this file
@@ -60,50 +67,87 @@ func (f *ScreenPrinter) Print(groupedTodos *GroupedTodos, printNotes bool) {
 }
 
 func (f *ScreenPrinter) printTodo(todo *Todo) {
-	if todo.IsPriority {
-		yellow.Add(color.Bold, color.Italic)
-	}
 	fmt.Fprintf(f.Writer, " %s\t%s\t%s\t%s\t\n",
-		yellow.Sprint(strconv.Itoa(todo.ID)),
+		f.formatID(todo.ID, todo.IsPriority),
 		f.formatCompleted(todo.Completed),
 		f.formatDue(todo.Due, todo.IsPriority, todo.Completed),
 		f.formatSubject(todo.Subject, todo.IsPriority))
 }
 
-func (f *ScreenPrinter) formatDue(due string, isPriority bool, completed bool) string {
+func (f *ScreenPrinter) formatID(ID int, isPriority bool) string {
 	if isPriority {
-		blue.Add(color.Bold, color.Italic)
-		red.Add(color.Bold, color.Italic)
+		return yellowBold.Sprint(strconv.Itoa(ID))
 	}
+	return yellow.Sprint(strconv.Itoa(ID))
+}
 
+func (f *ScreenPrinter) formatCompleted(completed bool) string {
+	if completed {
+		return "[x]"
+	}
+	return "[ ]"
+}
+
+func (f *ScreenPrinter) formatDue(due string, isPriority bool, completed bool) string {
 	if due == "" {
 		return nocolor.Sprint("          ")
 	}
 	dueTime, _ := time.Parse("2006-01-02", due)
 
-	if isToday(dueTime) {
-		return blue.Sprint("today     ")
-	} else if isTomorrow(dueTime) {
-		return blue.Sprint("tomorrow  ")
-	} else if isPastDue(dueTime) && !completed {
-		return red.Sprint(dueTime.Format("Mon Jan 02"))
+	if isPriority {
+		return f.printPriorityDue(dueTime, completed)
 	}
-	return blue.Sprint(dueTime.Format("Mon Jan 02"))
+	return f.printDue(dueTime, completed)
+
+}
+
+func (f *ScreenPrinter) printDue(due time.Time, completed bool) string {
+	if isToday(due) {
+		return blue.Sprint("today     ")
+	} else if isTomorrow(due) {
+		return blue.Sprint("tomorrow  ")
+	} else if isPastDue(due) && !completed {
+		return red.Sprint(due.Format("Mon Jan 02"))
+	}
+	return blue.Sprint(due.Format("Mon Jan 02"))
+}
+
+func (f *ScreenPrinter) printPriorityDue(due time.Time, completed bool) string {
+	if isToday(due) {
+		return blueBold.Sprint("today     ")
+	} else if isTomorrow(due) {
+		return blueBold.Sprint("tomorrow  ")
+	} else if isPastDue(due) && !completed {
+		return redBold.Sprint(due.Format("Mon Jan 02"))
+	}
+	return blueBold.Sprint(due.Format("Mon Jan 02"))
 }
 
 func (f *ScreenPrinter) formatSubject(subject string, isPriority bool) string {
-	if isPriority {
-		red.Add(color.Bold, color.Italic)
-		magenta.Add(color.Bold, color.Italic)
-		white.Add(color.Bold, color.Italic)
-	}
-
 	splitted := strings.Split(subject, " ")
-	projectRegex, _ := regexp.Compile(`\+[\p{L}\d_]+`)
-	contextRegex, _ := regexp.Compile(`\@[\p{L}\d_]+`)
 
+	if isPriority {
+		return f.printPrioritySubject(splitted)
+	}
+	return f.printSubject(splitted)
+}
+
+func (f *ScreenPrinter) printPrioritySubject(splitted []string) string {
 	coloredWords := []string{}
+	for _, word := range splitted {
+		if projectRegex.MatchString(word) {
+			coloredWords = append(coloredWords, magentaBold.Sprint(word))
+		} else if contextRegex.MatchString(word) {
+			coloredWords = append(coloredWords, redBold.Sprint(word))
+		} else {
+			coloredWords = append(coloredWords, whiteBold.Sprint(word))
+		}
+	}
+	return strings.Join(coloredWords, " ")
+}
 
+func (f *ScreenPrinter) printSubject(splitted []string) string {
+	coloredWords := []string{}
 	for _, word := range splitted {
 		if projectRegex.MatchString(word) {
 			coloredWords = append(coloredWords, magenta.Sprint(word))
@@ -114,11 +158,4 @@ func (f *ScreenPrinter) formatSubject(subject string, isPriority bool) string {
 		}
 	}
 	return strings.Join(coloredWords, " ")
-}
-
-func (f *ScreenPrinter) formatCompleted(completed bool) string {
-	if completed {
-		return "[x]"
-	}
-	return "[ ]"
 }
