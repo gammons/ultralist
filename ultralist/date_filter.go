fcc45cb3c7783b0a39ae836eee94155c410b3b15
Timo Runge <me@timorunge.com>
2019-03-19 14:10:35 +0100

    Simplify if condition.

--- ultralist/date_filter.go
+++ ultralist/date_filter.go
@@ -188,11 +188,9 @@ func (f *DateFilter) filterOverdue(pivot time.Time) []*Todo {
 	pivotDate := pivot.Format("2006-01-02")
 
 	for _, todo := range f.Todos {
-		if todo.Due != "" {
-			dueTime, _ := time.ParseInLocation("2006-01-02", todo.Due, f.Location)
-			if dueTime.Before(pivot) && pivotDate != todo.Due {
-				ret = append(ret, todo)
-			}
+		dueTime, _ := time.ParseInLocation("2006-01-02", todo.Due, f.Location)
+		if todo.Due != "" && dueTime.Before(pivot) && pivotDate != todo.Due {
+			ret = append(ret, todo)
 		}
 	}
 	return ret
