2c3db75d2086f7eb5c8f7f7ecbc68b2a4d6f9098
Grant Ammons <gammons@gmail.com>
2019-04-03 08:01:04 -0400

    fix test

--- ultralist/file_store_test.go
+++ ultralist/file_store_test.go
@@ -13,11 +13,11 @@ func TestFileStore(t *testing.T) {
 	store := &FileStore{FileLocation: testFilename}
 	defer testFileCleanUp(testFilename)
 	list.FindByID(2).Subject = "this is an non-fixture subject"
-	store.Save(list.Todos())
+	store.Save(list)
 
 	store1 := &FileStore{FileLocation: testFilename}
 
 	todos, _ := store1.Load()
-	assert.Equal(todos[1].Subject, "this is the first subject", "")
-	assert.Equal(todos[0].Subject, "this is an non-fixture subject", "")
+	assert.Equal(todos[0].Subject, "this is the first subject", "")
+	assert.Equal(todos[1].Subject, "this is an non-fixture subject", "")
 }
