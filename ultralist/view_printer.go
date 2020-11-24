0000b6c899aa31d0608d2d8d82572061e1d1ddaf
Grant Ammons <gammons@gmail.com>
2020-11-24 07:20:42 -0500

    add bold for priority

--- ultralist/view_printer.go
+++ ultralist/view_printer.go
@@ -9,7 +9,7 @@ import (
 type ViewPrinter struct{}
 
 func (v *ViewPrinter) FormatID(todo *Todo) string {
-	return fmt.Sprintf("[%s]%-3d[%s]", ColorYellow, todo.ID, ColorForeground)
+	return fmt.Sprintf("[%s::%s]%-3d[%s::-]", ColorYellow, v.bold(todo), todo.ID, ColorForeground)
 }
 
 func (v *ViewPrinter) FormatSubject(todo *Todo) string {
@@ -20,17 +20,17 @@ func (v *ViewPrinter) FormatSubject(todo *Todo) string {
 		if contextRegex.MatchString(word) {
 			coloredWords = append(
 				coloredWords,
-				fmt.Sprintf("[%s]%s[%s]", ColorRed, word, ColorForeground),
+				fmt.Sprintf("[%s::%s]%s[%s::-]", ColorRed, v.bold(todo), word, ColorForeground),
 			)
 		} else if projectRegex.MatchString(word) {
 			coloredWords = append(
 				coloredWords,
-				fmt.Sprintf("[%s]%s[%s]", ColorPurple, word, ColorForeground),
+				fmt.Sprintf("[%s::%s]%s[%s::-]", ColorPurple, v.bold(todo), word, ColorForeground),
 			)
 		} else {
 			coloredWords = append(
 				coloredWords,
-				fmt.Sprintf("[%s]%s[%s]", ColorForeground, word, ColorForeground),
+				fmt.Sprintf("[%s::%s]%s[%s::-]", ColorForeground, v.bold(todo), word, ColorForeground),
 			)
 		}
 	}
@@ -40,28 +40,35 @@ func (v *ViewPrinter) FormatSubject(todo *Todo) string {
 
 func (v *ViewPrinter) FormatCompleted(todo *Todo) string {
 	if todo.Completed {
-		return fmt.Sprintf("[%s][✔][%s]", ColorForeground, ColorForeground)
+		return fmt.Sprintf("[%s::%s][✔][%s::-]", ColorForeground, v.bold(todo), ColorForeground)
 	}
-	return fmt.Sprintf("[%s][ ][%s]", ColorForeground, ColorForeground)
+	return fmt.Sprintf("[%s::%s][ ][%s::-]", ColorForeground, v.bold(todo), ColorForeground)
 }
 
 func (v *ViewPrinter) FormatDue(todo *Todo) string {
 	due, _ := time.Parse(todo.Due, DATE_FORMAT)
 	if todo.DueToday() {
-		return fmt.Sprintf("[%s]%-10s[%s]", ColorBlue, "today", ColorForeground)
+		return fmt.Sprintf("[%s::%s]%-10s[%s::-]", ColorBlue, v.bold(todo), "today", ColorForeground)
 	} else if todo.DueTomorrow() {
-		return fmt.Sprintf("[%s]%-10s[%s]", ColorBlue, "tomorrow", ColorForeground)
+		return fmt.Sprintf("[%s::%s]%-10s[%s::-]", ColorBlue, v.bold(todo), "tomorrow", ColorForeground)
 	} else if todo.Due != "" && todo.PastDue() {
-		return fmt.Sprintf("[%s]%-10s[%s]", ColorRed, due.Format("Mon Jan 02"), ColorForeground)
+		return fmt.Sprintf("[%s::%s]%-10s[%s::-]", ColorRed, v.bold(todo), due.Format("Mon Jan 02"), ColorForeground)
 	}
 
 	if todo.Due == "" {
-		return ""
+		return fmt.Sprintf("%-10s", "")
 	} else {
-		return fmt.Sprintf("[%s]%-10s[%s]", ColorBlue, due.Format("Mon Jan 02"), ColorForeground)
+		return fmt.Sprintf("[%s::%s]%-10s[%s::-]", ColorBlue, v.bold(todo), due.Format("Mon Jan 02"), ColorForeground)
 	}
 }
 
 func (v *ViewPrinter) FormatStatus(todo *Todo) string {
-	return fmt.Sprintf("[%s]%-10s[%s]", ColorGreen, todo.Status, ColorForeground)
+	return fmt.Sprintf("[%s::%s]%-10s[%s::-]", ColorGreen, v.bold(todo), todo.Status, ColorForeground)
+}
+
+func (v *ViewPrinter) bold(todo *Todo) string {
+	if todo.IsPriority {
+		return "b"
+	}
+	return "-"
 }
